afb59213286b45d6f50fa2c485eada1bce41b604
René Schober <r.schober@outlook.com>
2025-03-05 12:26:25 +0100

    Create Main Repo

---
+++ UnderStandingTypescript/Section-11-ECMAScript_Decorators/decorators.ts
@@ -0,0 +1,60 @@
+function logger<T extends new (...args: any[]) => any>(
+    target: T, 
+    ctx: ClassDecoratorContext
+) {
+    console.log(target);
+    console.log(ctx);
+
+    return class extends target {
+        constructor(...args: any[]) {
+            super(...args);
+            console.log('class constructor');
+            console.log(this);
+        }
+    };
+}
+
+function autobind(
+    target: Function, 
+    ctx: ClassMethodDecoratorContext
+) {
+    ctx.addInitializer(function(this: any) {
+        this[ctx.name] = this[ctx.name].bind(this);
+    });
+
+    return function(this: any) {
+        console.log('Executing original function')
+        target.apply(this);
+    }
+}
+
+function replacer<T>(initValue: T) {
+    return function (
+        target: undefined, 
+        ctx: ClassFieldDecoratorContext
+    ) {
+        console.log(target);
+        console.log(ctx);
+
+        return (initialValue: any) => {
+            console.log( initialValue );
+            return initValue;
+        } 
+    }
+}
+
+@logger
+class Person {
+    @replacer('')
+    name = 'René';
+
+    @autobind
+    greet() {
+        console.log('Hi, I am ' + this.name);
+    }
+}
+
+const rene = new Person();
+const greet = rene.greet;
+
+greet();
